17a54318113670708312ea9c72ea9400677ba811
Komssa77 <connorforster2000@gmail.com>
2024-03-19 01:39:05 +1100

    DynamoDB functions improved, includes initializer code for deployment

--- dynoTest.py
+++ dynoTest.py
@@ -14,52 +14,164 @@ responseIAM = iam_client.list_users()
 #print(responseIAM)
 
 
-dynamodb_client = boto3.client('dynamodb', region_name='ap-southeast-2')
-responseDB = dynamodb_client.list_tables()
-print(responseDB['TableNames'])
+#dynamodb_client = boto3.client('dynamodb', region_name='ap-southeast-2')
+#responseDB = dynamodb_client.list_tables()
+#print(responseDB['TableNames'])
 
+
+
+
+#creating the user table
+def depreciate():
+    dynamodb_client = boto3.resource('dynamodb', region_name='ap-southeast-2')
+
+    try: 
+        newTable = dynamodb_client.create_table(
+            TableName='depreciate',
+            KeySchema=[
+            {
+                'AttributeName': 'username',
+                'KeyType': 'HASH'  # HASH for the primary key
+            },
+            {
+                'AttributeName': 'email',
+                'KeyType': 'RANGE'  # RANGE for the sort key
+            },
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'username',
+                'AttributeType': 'S'  # S for String type
+            },
+            {
+                'AttributeName': 'email',
+                'AttributeType': 'S'  # S for String type
+            },
+            #{
+            #    'AttributeName': 'password',
+            #    'AttributeType': 'S'  # S for String type
+            #}
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5
+        }
+        )
+
+        newTable.wait_until_exists()
+        print("complete")
+        #initItems()
+
+    except dynamodb_client.exceptions.ResourceInUseException:
+        print("users table already exists")
+
+
+
+
+
+
+def initFunctions():
+    dynamodb_client = boto3.client('dynamodb', region_name='ap-southeast-2')
+    responseDB = dynamodb_client.list_tables()
+    #print(responseDB)
+
+    if 'Init_users' not in responseDB['TableNames']:
+        createTable()
+    else:
+        print("tables already initialized")
 
 
 
 
 def createTable():
     dynamodb_client = boto3.resource('dynamodb', region_name='ap-southeast-2')
+
+    
     newTable = dynamodb_client.create_table(
-        TableName='users',
+        TableName='Init_users',
         KeySchema=[
-        {
-            'AttributeName': 'username',
-            'KeyType': 'HASH'  # HASH for the primary key
-        },
-        {
-            'AttributeName': 'email',
-            'KeyType': 'RANGE'  # RANGE for the sort key
-        },
-    ],
+            {
+                'AttributeName': 'username',
+                'KeyType': 'HASH'  # HASH for the primary key
+            },
+            {
+                'AttributeName': 'email',
+                'KeyType': 'RANGE'  # RANGE for the sort key
+            },
+        ],
     AttributeDefinitions=[
-        {
-            'AttributeName': 'username',
-            'AttributeType': 'S'  # S for String type
-        },
-        {
-            'AttributeName': 'email',
-            'AttributeType': 'S'  # S for String type
-        },
-        #{
-        #    'AttributeName': 'password',
-        #    'AttributeType': 'S'  # S for String type
-        #}
-    ],
+            {
+                'AttributeName': 'username',
+                'AttributeType': 'S'  # S for String type
+            },
+            {
+                'AttributeName': 'email',
+                'AttributeType': 'S'  # S for String type
+            },
+           
+        ],
     ProvisionedThroughput={
-        'ReadCapacityUnits': 5,
-        'WriteCapacityUnits': 5
-    }
-    )
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5
+        }
+        )
 
     newTable.wait_until_exists()
-    print(newTable.item_count)
-    
+    print("table created")
+    initItems()
 
+    
+def initItems():
+
+    dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2')
+    table = dynamodb.Table('Init_users')
+    initItemsList=[{
+            'username': 'CF0',
+            'email': 's37814750@student.rmit.edu.au',
+            'password': '012345',
+        },{
+            'username': 'CF1',
+            'email': 's37814751@student.rmit.edu.au',
+            'password': '123456',
+        },{
+            'username': 'CF2',
+            'email': 's37814752@student.rmit.edu.au',
+            'password': '234567',
+        },{
+            'username': 'CF3',
+            'email': 's37814753@student.rmit.edu.au',
+            'password': '345678',
+        },{
+            'username': 'CF4',
+            'email': 's37814754@student.rmit.edu.au',
+            'password': '456789',
+        },{
+            'username': 'CF5',
+            'email': 's37814755@student.rmit.edu.au',
+            'password': '567890',
+        },{
+            'username': 'CF6',
+            'email': 's37814756@student.rmit.edu.au',
+            'password': '678901',
+        },{
+            'username': 'CF7',
+            'email': 's37814757@student.rmit.edu.au',
+            'password': '789012',
+        },{
+            'username': 'CF8',
+            'email': 's37814758@student.rmit.edu.au',
+            'password': '890123',
+        },{
+            'username': 'CF9',
+            'email': 's37814759@student.rmit.edu.au',
+            'password': '901234',
+        }
+        ]
+    
+    for item in initItemsList:
+        table.put_item(Item = item)
+    
+    print("user table items initialized")
 
 
 
@@ -138,5 +250,27 @@ def checkLoginDetails(usernameInput, passwordInput):
         return False
 
 
-checkLoginDetails('User1','Pass2')
-createTable()
+def new():
+    myWords = ["Not","everything","that","counts","can","be","counted","and","not","everything","that","can","be","counted","counts"]
+    count = len(myWords)
+
+    if count % 2 == 1: #odd
+        point = int ((count-1)/2)
+
+        print(myWords[:point])
+        print(myWords[point:])
+
+    else: # even
+        point = int (count/2)
+
+        print(myWords[:point])
+        print(myWords[point:])
+
+
+    mid = len(myWords) // 2
+    print(myWords[:mid])
+    print(myWords[mid:])
+
+#checkLoginDetails('User1','Pass2')
+initFunctions()
+#new()
